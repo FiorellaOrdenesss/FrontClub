6023629b563c1bd6905806f82af0a484eef3c6a3
FiorellaOrdenesss <fiordenesvt@gmail.com>
2026-05-29 21:09:07 -0300

    Todo listo para empezar yuli

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// ESTO ES MAIN, CUIDADO AL REALIZAR CAMBIOS
+// Hola Yuli
 import "./index.css";
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-red-600 to-black">
       <h1 className="text-5xl font-extrabold text-white animate-pulse">
-        Tailwind funcionando en fio ✨
+        Tailwind funcionando en yuli ✨
       </h1>
     </div>
   );
