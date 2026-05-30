fbd621aac33cd6c755ba75a8c168ba51a6f87de1
FiorellaOrdenesss <fiordenesvt@gmail.com>
2026-05-29 21:11:18 -0300

    Todo listo para empezar Leo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-// Hola Yuli
+// Hola Leo
 import "./index.css";
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-red-600 to-black">
       <h1 className="text-5xl font-extrabold text-white animate-pulse">
-        Tailwind funcionando en yuli ✨
+        Tailwind funcionando en leonela ✨
       </h1>
     </div>
   );
